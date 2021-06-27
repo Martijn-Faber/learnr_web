724f6b6562e6a58f49661acd7cf1822f13604ec9
Martijn Faber <martijn@fabtronics.nl>
2021-06-27 15:47:34 +0200

    improvement(header): add background color and borders

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,20 +1,33 @@
 <template>
-  <Container>
-    <header>
-      <div>
-        <router-link :to="{ name: 'dashboard' }">Dashboard</router-link>
-      </div>
-      <div><Avatar /></div>
-    </header>
-  </Container>
+  <div class="header">
+    <Container>
+      <header>
+        <div>
+          <router-link :to="{ name: 'dashboard' }">Dashboard</router-link>
+        </div>
+        <div><Avatar /></div>
+      </header>
+    </Container>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 25px 0;
+.header {
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+
+  background-color: var(--body-lighter);
+  padding: var(--gap-small) 0;
+  margin-bottom: var(--gap);
+  border-bottom: var(--border);
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 }
 </style>
 
