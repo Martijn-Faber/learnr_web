417b447bb446d4b4810b8c593fd55aa1bdd184ad
Martijn Faber <martijn@fabtronics.nl>
2021-06-27 10:29:32 +0200

    feat(dashboard): add container component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,17 @@
 <template>
-  <h1>Hello world</h1>
+  <Container>
+    <h1>Hello world</h1>
+  </Container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Container from "@/components/layout/Container.vue";
 
 export default defineComponent({
   name: "Home",
+  components: {
+    Container,
+  },
 });
 </script>
