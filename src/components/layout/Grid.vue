415a607101a4703045cbc21c35fe016f54d64a81
Martijn Faber <martijn@fabtronics.nl>
2021-06-28 16:10:46 +0200

    feat(grid): add grid component

---
+++ src/components/layout/Grid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="grid">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: var(--gap);
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Grid",
+});
+</script>
