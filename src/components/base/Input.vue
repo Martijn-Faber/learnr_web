ff89bd0fc1ccf6d84e8516ff3dbdb1096e5e10b7
Martijn Faber <martijn@fabtronics.nl>
2021-06-28 16:09:49 +0200

    feat(input): add input component

---
+++ src/components/base/Input.vue
@@ -0,0 +1,41 @@
+<template>
+  <input
+    class="input"
+    :value="value"
+    @blur="$emit('blur', $event.target.value)"
+    @change="$emit('value', $event.target.value)"
+    :placeholder="placeholder"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Input",
+  props: {
+    value: {
+      type: String,
+    },
+    placeholder: {
+      type: String,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.input {
+  width: 100%;
+  padding: 8px 20px;
+  min-height: 50px;
+  border-radius: var(--border-radius);
+  background-color: var(--body-lighter);
+  border: var(--border);
+  color: var(--text);
+  transition: all 0.2s ease-in-out;
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
