49a1ceb0a27dc1196e403aece219b69d8d52c44b
Martijn Faber <martijn@fabtronics.nl>
2021-06-28 16:17:34 +0200

    fix(router): add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,41 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Dashboard from "@/views/Dashboard.vue";
+import Set from "@/views/Set.vue";
+import Edit from "@/views/Edit.vue";
+import Create from "@/views/Create.vue";
+import Search from "@/views/Search.vue";
+import User from '@/views/User.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "dashboard",
     component: Dashboard,
+  },
+  {
+    path: "/set/:id",
+    name: "set",
+    component: Set,
+  },
+  {
+    path: "/set/:id/edit",
+    name: "edit",
+    component: Edit,
+  },
+  {
+    path: "/set/create",
+    name: "create",
+    component: Create,
+  },
+  {
+    path: "/user/:id",
+    name: "user",
+    component: User,
+  },
+  {
+    path: "/search",
+    name: "search",
+    component: Search,
   },
 ];
 
