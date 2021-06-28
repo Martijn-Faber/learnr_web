9931318d420cbcec68e6e778f6251aa9474c2e6a
Martijn Faber <martijn@fabtronics.nl>
2021-06-28 16:13:54 +0200

    feat(dashboard): add dashboard page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,18 @@
 <template>
   <Header />
   <Container>
-    <h1>Hello world</h1>
+    <Button @click="$router.push({ name: 'create' })">Create set</Button>
+    <h2 class="mb-gap">Recent</h2>
+    <Grid>
+      <SetCard
+        v-for="(set, index) in sets.reverse()"
+        :key="index"
+        :id="set.id"
+        :title="set.title"
+        :description="set.description"
+        :author="set.author"
+      ></SetCard>
+    </Grid>
   </Container>
 </template>
 
@@ -9,12 +20,35 @@
 import { defineComponent } from "vue";
 import Container from "@/components/layout/Container.vue";
 import Header from "@/components/layout/Header.vue";
+import Grid from "@/components/layout/Grid.vue";
+
+import { api } from "@/config/api";
+import SetCard from "@/components/base/SetCard.vue";
+import Button from "@/components/base/Button.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
     Container,
     Header,
+    Grid,
+    SetCard,
+    Button,
+  },
+  data() {
+    return {
+      sets: [],
+    };
+  },
+  methods: {
+    async getSets() {
+      const sets = await api.get("/sets");
+      this.sets = sets.data.data;
+      console.log(this.sets);
+    },
+  },
+  created() {
+    this.getSets();
   },
 });
 </script>
