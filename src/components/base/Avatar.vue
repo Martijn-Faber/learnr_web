bc8854b693cb1a045c7742a4aca52c88992b7a3c
Martijn Faber <martijn@fabtronics.nl>
2021-06-28 15:00:04 +0200

    improvement(avatar): fix background size and repeat and add a big size

--- src/components/base/Avatar.vue
+++ src/components/base/Avatar.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="avatar" :style="{ backgroundImage: `url(${url})` }"></div>
+  <div
+    class="avatar"
+    :style="{ backgroundImage: `url(${url})` }"
+    :data-size="size"
+  ></div>
 </template>
 
 <style lang="scss" scoped>
@@ -7,6 +11,16 @@
   width: 50px;
   height: 50px;
   border-radius: 50%;
+  border: var(--border);
+
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+
+  &[data-size="big"] {
+    width: 100px;
+    height: 100px;
+  }
 }
 </style>
 
@@ -15,6 +29,11 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Avatar",
+  props: {
+    size: {
+      type: String,
+    },
+  },
   data() {
     return {
       url: "https://avatars.githubusercontent.com/u/69846460?s=48&v=4", //TODO: fetch avatar from api
